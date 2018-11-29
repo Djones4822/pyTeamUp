c18228fc604458ae928130d39d223dc7ce44d623
David Jones <David.jones4822@gmail.com>
2018-11-29 13:31:10 -0500

    fixed init bug

--- pyTeamUp/__init__.py
+++ pyTeamUp/__init__.py
@@ -9,6 +9,7 @@ Python Library for interfacing with the TeamUp api
 __version__ = '0.0.1a'
 __author__ = 'David Jones'
 
+from pyTeamUp.Calendar import Calendar
 from pyTeamUp.Event import Event
 
 # Set default logging handler to avoid "No handler found" warnings.  ## STOLEN FROM https://github.com/nithinmurali/pygsheets/blob/staging/pygsheets/__init__.py thanks Nithin :)
