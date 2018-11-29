c65f31ba26bb0336e9f83b7672bb482f00e58858
David Jones <David.jones4822@gmail.com>
2018-11-29 18:18:52 -0500

    Moved API endpoint strings to constants.py

---
+++ pyTeamUp/utils/constants.py
@@ -0,0 +1,6 @@
+BASE_URL = f'https://api.teamup.com'
+CHECK_ACCESS_BASE = '/check-access'
+EVENTS_BASE = '/events'
+SUBCALENDARS_BASE = '/subcalendars'
+CONFIGURATION_BASE = '/configuration'
+POST_HEADERS = {'Content-type': 'application/json'}
