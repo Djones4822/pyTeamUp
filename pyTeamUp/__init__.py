6d48fd4e919270bb25ab1b89c7008e8158665b60
David Jones <David.jones4822@gmail.com>
2018-11-29 13:25:56 -0500

    organizing folders

--- pyTeamUp/__init__.py
+++ pyTeamUp/__init__.py
@@ -9,8 +9,7 @@ Python Library for interfacing with the TeamUp api
 __version__ = '0.0.1a'
 __author__ = 'David Jones'
 
-from Calendar import Calendar
-from Event import Event
+from pyTeamUp.Event import Event
 
 # Set default logging handler to avoid "No handler found" warnings.  ## STOLEN FROM https://github.com/nithinmurali/pygsheets/blob/staging/pygsheets/__init__.py thanks Nithin :)
 import logging
