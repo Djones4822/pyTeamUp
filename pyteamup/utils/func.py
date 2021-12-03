cb9296813311bf3d51291df0ff4951c441d65fe6
Djones4822 <david.jones4822@gmail.com>
2021-12-03 17:38:44 -0500

    finish `make_request` function fix bug in check_status_code add docstrings

--- pyteamup/utils/func.py
+++ pyteamup/utils/func.py
@@ -1,5 +1,9 @@
 """
-functions for pyteamup
+Utility Functions for PyTeamUp
+
+Author: David Jones
+Creation Date: 11/27/2017
+Last Updated 12/3/2021
 """
 import datetime
 from pyteamup.utils.const import RESPONSES
@@ -15,18 +19,32 @@ logger = logging.getLogger(__name__)
 
 def check_status_code(status_code, response=None, url=None, headers=None, fp=None):
     """
-    Utility function for checking the result of a response. requires status code, pass the response to get
+    Utility function for checking the result of a Teamup API response. requires status code, pass the response to get
     the server message as a TeamUpError rather than HTTPError. Otherwise returns the message.
+
+    Not intended for any other use than the TeamUp API
+
+    Parameters
+    ----------
+    status_code - required, should be from response.status_code
+    response - requests.response object OR python dictionary of decoded response JSON
+    url - optional, for HTTPError which is raised if the response is malformed or not from Teamup
+    headers - optional, for HTTPError which is raised if the response is malformed or not from Teamup
+    fp - optional, for HTTPError which is raised if the response is malformed or not from Teamup
+
+    Returns
+    -------
+
     """
     if status_code >= 400:
-        if not isinstance(response, dict):
+        if isinstance(response, requests.Response):
             try:
-                response = json.loads(response)
+                response = json.loads(response.text)
             except JSONDecodeError as e:
                 logger.error('Cannot decode response')
                 logger.exception(e)
                 response = None
-        if response:
+        if response and 'error' in response.keys():
             error_msg = response['error']['message']
             error_title = response['error']['title']
             raise TeamUpError(f'Response {status_code} - {error_title}: {error_msg}')
@@ -35,6 +53,16 @@ def check_status_code(status_code, response=None, url=None, headers=None, fp=Non
 
 
 def format_date(date):
+    """
+    Helper to easily format dates with or without timezones.
+    Parameters
+    ----------
+    date - datetime object
+
+    Returns
+    -------
+    string containing formatted text, uses mask: %Y-%m-%dT%H:%M:%S%z
+    """
     if not isinstance(date, datetime.datetime):
         raise TypeError
     if date.tzinfo is None:
@@ -42,14 +70,41 @@ def format_date(date):
     else:
         return date.strftime('%Y-%m-%dT%H:%M:%S%z')
 
-def make_request(method, url, headers, payload=None):
-    if method.lower() == 'get':
-        req = requests.get(url, headers=headers)
-    if method.lower() == 'post':
-        req = requests.post(url, headers=headers, data=payload)
-    if method.lower() == 'put':
-        req = requests.put(url, headers=headers, data=payload)
-    if method.lower() == 'delete':
-        req = requests.put(url, headers=headers, data=payload)
-    check_status_code(req)
-    return req
+
+def make_request(method, url, headers=None, payload=None):
+    """
+    Function to make requests - intended for teamup only, checks the response using `check_status_code()`.
+
+    Added to reduce code duplication.
+
+    Parameters
+    ----------
+    method
+    url
+    headers - python dictionary of header options
+    payload - json string, do not pass a python dictionary
+
+    Returns
+    -------
+    requests.Response object
+    """
+    m = method.lower()
+    if m == 'get':
+        resp = requests.get(url, headers=headers)
+    elif m == 'post':
+        resp = requests.post(url, headers=headers, data=payload)
+    elif m == 'put':
+        resp = requests.put(url, headers=headers, data=payload)
+    elif m == 'delete':
+        resp = requests.delete(url, headers=headers)
+    else:
+        raise ValueError(f'Unknown Method type {method}')
+
+    try:
+        check_status_code(resp.status_code, resp)
+    except TeamUpError as e:
+        if payload:
+            logger.error(f'Payload sent with request:\n{payload}')
+        raise e
+
+    return resp
